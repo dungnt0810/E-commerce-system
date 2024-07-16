f078bc9d66ff7302030e51820913f872f793d4e9
dungnt <dung.nguyen.tien.0810@gmail.com>
2024-07-16 14:59:31 +0700

    ui: header css

--- store-ui/src/components/Layouts/Header/Header.js
+++ store-ui/src/components/Layouts/Header/Header.js
@@ -1,12 +1,26 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faCartShopping, faCircleXmark, faSearch } from '@fortawesome/free-solid-svg-icons';
 
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
+import { faHeart, faUser } from '@fortawesome/free-regular-svg-icons';
+import { useRef, useState } from 'react';
 
 const cx = classNames.bind(styles);
 
 function Header() {
+    const [searchQuery, setSearchQuery] = useState('');
+    const inputRef = useRef();
+
+    const handleSearchChange = (e) => {
+        setSearchQuery(e.target.value);
+    };
+
+    const handleSearchClick = () => {
+        // Handle the search click event here
+        console.log('Search query:', searchQuery);
+    };
+
     return (
         <div className={cx('wrapper')}>
             <div className={cx('inner')}>
@@ -28,16 +42,40 @@ function Header() {
 
                 <div className={cx('action')}>
                     <div className={cx('search')}>
-                        <input type="text" placeholder="Search accounts and video" spellCheck="false" />
-                        <button className={cx('clear-btn')}>
-                            <FontAwesomeIcon icon={faCircleXmark} />
-                        </button>
-                        <button className={cx('search-btn')}>
+                        <input
+                            type="text"
+                            placeholder="What are you looking for ?"
+                            spellCheck="false"
+                            value={searchQuery}
+                            ref={inputRef}
+                            onChange={handleSearchChange}
+                        />
+                        {!!searchQuery && (
+                            <button
+                                className={cx('clear-btn')}
+                                onClick={() => {
+                                    inputRef.current.focus();
+                                    setSearchQuery('');
+                                }}
+                            >
+                                <FontAwesomeIcon icon={faCircleXmark} />
+                            </button>
+                        )}
+                        <button className={cx('search-btn')} onClick={handleSearchClick}>
                             <FontAwesomeIcon icon={faSearch} />
                         </button>
                     </div>
-                    <button>Favourite</button>
-                    <button>Cart</button>
+                    <button className={cx('fav-btn')}>
+                        <FontAwesomeIcon icon={faHeart} />
+                    </button>
+                    <button className={cx('cart-btn')}>
+                        <FontAwesomeIcon icon={faCartShopping} className={cx('cart-icon')} />
+                        <div className={cx('badge')}>0</div>
+                    </button>
+
+                    <button className={cx('user-btn')}>
+                        <FontAwesomeIcon icon={faUser} />
+                    </button>
                 </div>
                 {/* Action Area */}
             </div>
